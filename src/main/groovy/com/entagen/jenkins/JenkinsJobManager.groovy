6efd53b559e08dd4858260401496323549debced
Derek Hansen <dhansen@nimbleuser.com>
2013-08-09 10:22:53 -0400

    Use branchNameRegex for delete matching

--- src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
@@ -110,7 +110,7 @@ class JenkinsJobManager {
     }
 
     List<TemplateJob> findRequiredTemplateJobs(List<String> allJobNames) {
-        String regex = /^($templateJobPrefix-[^-]*)-($templateBranchName)$/
+        String regex = /^($templateJobPrefix-[^-]*)-($branchNameRegex)$/
 
         List<TemplateJob> templateJobs = allJobNames.findResults { String jobName ->
             TemplateJob templateJob = null
